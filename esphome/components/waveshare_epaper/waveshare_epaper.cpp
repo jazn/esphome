03d950d4af84cb395315bfa684fd8a104d3b29fc
Mateusz [Jazn] Niemojewski <mojes@wp.pl>
2023-01-12 15:21:15 +0100

    Fix.

--- esphome/components/waveshare_epaper/waveshare_epaper.cpp
+++ esphome/components/waveshare_epaper/waveshare_epaper.cpp
@@ -479,6 +479,16 @@ uint32_t WaveshareEPaperTypeA::idle_timeout_() {
     0x00,0x00,0x00,0x00,0x22,0x22,0x22,0x22,0x22,0x22,
     0x00,0x00,0x00 // this was in https://github.com/waveshare/e-Paper/blob/master/Arduino/epd2in66/epd2in66.cpp ,0x22,0x17,0x41,0xB0,0x32,0x36,
  };
+  void WaveshareEPaper::reset_() {
+  ESP_LOGD(TAG, "reset_");
+
+    if (this->reset_pin_ != nullptr) {
+      this->reset_pin_->digital_write(false);
+      delay(reset_duration_);  // NOLINT
+      this->reset_pin_->digital_write(true);
+      delay(200);  // NOLINT
+    }
+  }
 
 void WaveshareEPaper2P66In::initialize() {
   ESP_LOGD(TAG, "initialize");
